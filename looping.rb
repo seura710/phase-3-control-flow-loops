fb21c65aea19e4f5d282a76813f160ba5edfbef8
Harris Kim <seura710@gmail.com>
2021-11-23 19:01:32 -0500

    commit tree 212d2406b432f0d8aa96abccbaa8a1db920e1674

--- looping.rb
+++ looping.rb
@@ -1,7 +1,11 @@
 def happy_new_year
-  # your code here
+  counter = 10 
+  until counter == 0
+    puts counter
+    counter -= 1
 end
-
+puts "Happy New Year!"
+end
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
 def fizzbuzz(num)
   if num % 3 == 0 && num % 5 == 0
@@ -16,9 +20,24 @@ def fizzbuzz(num)
 end
 
 def fizzbuzz_printer
-  # your code here
+  counter = 1
+  until counter == 100
+  if counter % 3 == 0
+    puts "Fizz"
+  elsif counter % 5 == 0
+    puts "Buzz"
+  elsif counter % 3 && counter % 5 == 0 
+    puts "FizzBuzz"
+  else puts counter
+  counter += 1
+  end
 end
 
-def reverse_string(str)
-  # your code here
-end
+# Deliverables 
+# //Write a method #fizzbuzz that prints the numbers from 1 to 100. For multiples of three, print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five, print "FizzBuzz".
+
+
+
+# def reverse_string(str)
+  
+# end
